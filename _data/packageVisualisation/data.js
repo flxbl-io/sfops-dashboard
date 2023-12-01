6ae7c1449b660181ca55271ccd8ba12b3381d89e
sfopsbot <sfopsbot@flxbl.io>
2023-12-01 10:37:47 +0000

    Updated information about a branch

--- _data/packageVisualisation/data.js
+++ _data/packageVisualisation/data.js
@@ -1,1 +1,680 @@
-const dataJson = null
+const dataJson = [
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-16T00:17:17+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-16T00:17:17+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-15T23:43:16+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-15T23:43:16+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-15T23:15:49+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-15T23:15:49+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-15T21:24:29+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-15T21:24:29+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-15T20:09:49+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-15T20:09:49+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-15T17:46:04+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-15T17:46:04+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-15T15:52:02+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-15T15:52:02+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-13T13:30:34+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-13T13:30:34+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-07T20:08:35+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-07T20:08:35+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-04T13:43:23+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-04T13:43:23+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-04T13:31:03+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-04T13:31:03+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-03T23:52:46+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-03T23:52:46+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-03T23:48:22+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-03T23:48:22+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-11-02T16:16:25+11:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-11-02T16:16:25+11:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-09-19T17:39:14+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-09-19T17:39:14+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-09-15T14:36:44+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-09-15T14:36:44+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-09-07T20:19:36+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-09-07T20:19:36+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-09-04T10:01:40+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-09-04T10:01:40+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-31T05:41:50+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-31T05:41:50+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-30T21:41:55+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-30T21:41:55+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-30T19:19:47+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-30T19:19:47+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-28T18:38:04+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T18:38:04+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-28T18:17:48+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T18:17:48+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-28T17:59:13+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T17:59:13+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-28T17:29:30+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T17:29:30+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-28T17:03:22+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T17:03:22+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-28T16:34:16+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T16:34:16+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-28T16:06:27+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T16:06:27+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-28T15:18:33+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T15:18:33+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-28T11:54:43+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T11:54:43+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-28T12:17:41+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T12:17:41+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T05:40:35+10:00",
+      "fileCount": 2
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-28T05:17:41+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T05:17:41+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T05:17:41+10:00",
+      "fileCount": 2
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-28T04:58:47+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T04:58:47+10:00",
+      "fileCount": 3
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-28T02:28:14+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-28T02:28:14+10:00",
+      "fileCount": 2
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "feature-management",
+      "size": 18,
+      "domain": "frameworks",
+      "when": "2023-08-27T12:22:48+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-27T12:22:48+10:00",
+      "fileCount": 2
+    }
+  ],
+  [
+    {
+      "path": "src/frameworks/feature-management",
+      "package": "src/frameworks/feature-management",
+      "size": 33,
+      "domain": "",
+      "when": "2023-08-27T11:57:41+10:00",
+      "fileCount": 40
+    },
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-27T11:57:41+10:00",
+      "fileCount": 2
+    }
+  ],
+  [
+    {
+      "path": "unpackaged",
+      "package": "unpackaged",
+      "size": 10,
+      "domain": "unpackaged",
+      "when": "2023-08-27T10:39:04+10:00",
+      "fileCount": 2
+    }
+  ]
+]
